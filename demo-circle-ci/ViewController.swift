d396af7402d8449745e46e34082e4dbe02e8948d
phonglh-2668 <le.hung.phong@sun-asterisk.com>
2022-10-27 23:17:11 +0700

    Initial Commit

---
+++ demo-circle-ci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo-circle-ci
+//
+//  Created by le.hung.phong on 27/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
